72269ead1adc0b678f072a10123d33072e2efd30
Kevin Scroggins <nitro404@gmail.com>
2010-10-31 06:30:03 +0000

    Added skeleton GUI for Client contact list / main interface window Added skeleton GUI for Client conversation window
    git-svn-id: file:///e/SVN/serious_messenger@16 b1debafe-a605-4e1b-80f9-83a58a4e6cd1

--- Serious Client/src/client/Client.java
+++ Serious Client/src/client/Client.java
@@ -1,13 +1,228 @@
 package client;
 
-public class Client {
+import javax.swing.*;
+import javax.swing.tree.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Client extends JFrame {
 	
-	public Client() {
-		
-	}
+	private JMenuBar menuBar;
 	
-	public void initialize() {
-		
-	}
+	private JMenu fileMenu;
+    private JMenuItem fileSignInMenuItem;
+    private JMenuItem fileSignOutMenuItem;
+    private JMenuItem fileExitMenuItem;
 	
+    private JMenu contactsMenu;
+    private JMenuItem contactsAddContactMenuItem;
+    private JMenuItem contactsGreateGroupMenuItem;
+    
+	private JScrollPane contactListScrollPane;
+    private JTree contactListTree;
+    
+    private JTextField nickNameTextField;
+    private JTextField personalMessageTextField;
+    private JComboBox statusComboBox;
+	
+    private static final long serialVersionUID = 1L;
+    
+    public Client() {
+        initComponents();
+        
+        fileSignOutMenuItem.setEnabled(false);
+        contactsAddContactMenuItem.setEnabled(false);
+        contactsGreateGroupMenuItem.setEnabled(false);
+        nickNameTextField.setEnabled(false);
+        statusComboBox.setEnabled(false);
+        personalMessageTextField.setEnabled(false);
+    }
+    
+    public void initialize() {
+    	setVisible(true);
+    }
+    
+    private void initComponents() {
+    	setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Serious Messenger");
+        setMinimumSize(new Dimension(100, 200));
+        
+        menuBar = new JMenuBar();
+        
+        fileMenu = new JMenu();
+        fileSignInMenuItem = new JMenuItem();
+        fileSignOutMenuItem = new JMenuItem();
+        fileExitMenuItem = new JMenuItem();
+        
+        contactsMenu = new JMenu();
+        contactsAddContactMenuItem = new JMenuItem();
+        contactsGreateGroupMenuItem = new JMenuItem();
+        
+        contactListScrollPane = new JScrollPane();
+        contactListTree = new JTree();
+        
+        nickNameTextField = new JTextField();
+        personalMessageTextField = new JTextField();
+        statusComboBox = new JComboBox();
+
+        nickNameTextField.setText("Nickname");
+        nickNameTextField.addFocusListener(new FocusAdapter() {
+            public void focusLost(FocusEvent evt) {
+                nickNameTextFieldFocusLost(evt);
+            }
+        });
+        nickNameTextField.addKeyListener(new KeyAdapter() {
+            public void keyReleased(KeyEvent evt) {
+                nickNameTextFieldKeyReleased(evt);
+            }
+        });
+
+        personalMessageTextField.setText("Personal Message");
+        personalMessageTextField.addFocusListener(new FocusAdapter() {
+            public void focusLost(FocusEvent evt) {
+                personalMessageTextFieldFocusLost(evt);
+            }
+        });
+        personalMessageTextField.addKeyListener(new KeyAdapter() {
+            public void keyReleased(KeyEvent evt) {
+                personalMessageTextFieldKeyReleased(evt);
+            }
+        });
+
+        statusComboBox.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent evt) {
+                statusComboBoxItemStateChanged(evt);
+            }
+        });
+
+        //DefaultMutableTreeNode treeNode1 = new DefaultMutableTreeNode();
+        //contactListTree.setModel(new DefaultTreeModel(treeNode1));
+        contactListTree.setModel(null);
+        contactListTree.addMouseListener(new MouseAdapter() {
+            public void mouseReleased(MouseEvent evt) {
+                contactListTreeMouseReleased(evt);
+            }
+        });
+        contactListScrollPane.setViewportView(contactListTree);
+
+        fileMenu.setText("File");
+
+        fileSignInMenuItem.setText("Sign In");
+        fileSignInMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                fileSignInMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(fileSignInMenuItem);
+
+        fileSignOutMenuItem.setText("Sign Out");
+        fileSignOutMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                fileSignOutMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(fileSignOutMenuItem);
+
+        fileExitMenuItem.setText("Exit");
+        fileExitMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                fileExitMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(fileExitMenuItem);
+
+        menuBar.add(fileMenu);
+
+        contactsMenu.setText("Contacts");
+
+        contactsAddContactMenuItem.setText("Add Contact");
+        contactsAddContactMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                contactsAddContactMenuItemActionPerformed(evt);
+            }
+        });
+        contactsMenu.add(contactsAddContactMenuItem);
+
+        contactsGreateGroupMenuItem.setText("Create Group");
+        contactsGreateGroupMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                contactsGreateGroupMenuItemActionPerformed(evt);
+            }
+        });
+        contactsMenu.add(contactsGreateGroupMenuItem);
+
+        menuBar.add(contactsMenu);
+
+        setJMenuBar(menuBar);
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(nickNameTextField, GroupLayout.DEFAULT_SIZE, 204, Short.MAX_VALUE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(statusComboBox, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE))
+            .addComponent(personalMessageTextField, GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
+            .addComponent(contactListScrollPane, GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(contactListScrollPane, GroupLayout.DEFAULT_SIZE, 467, Short.MAX_VALUE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(nickNameTextField, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(statusComboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(personalMessageTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        );
+
+        pack();
+    }
+    
+    private void fileSignInMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void fileSignOutMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void fileExitMenuItemActionPerformed(ActionEvent evt) {
+        System.exit(0);
+    }
+    
+    private void contactsAddContactMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void contactsGreateGroupMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void statusComboBoxItemStateChanged(ItemEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void personalMessageTextFieldFocusLost(FocusEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void nickNameTextFieldFocusLost(FocusEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void personalMessageTextFieldKeyReleased(KeyEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void nickNameTextFieldKeyReleased(KeyEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void contactListTreeMouseReleased(MouseEvent evt) {
+        // TODO add your handling code here:
+    }
+    
 }
--- Serious Client/src/client/Conversation.java
+++ Serious Client/src/client/Conversation.java
@@ -1,12 +1,47 @@
 package client;
 
 import java.util.Vector;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Conversation {
+public class Conversation extends JFrame {
 	
 	private Vector<Message> m_messages;
 	private User m_user;
 	private Vector<Contact> m_participants;
+    
+	private JMenuBar messageMenuBar;
+	
+	private JMenu fileMenu;
+	private JMenuItem fileSaveConversationMenuItem;
+    private JMenuItem fileViewHistoryMenuItem;
+	private JMenuItem fileCloseMenuItem;
+    
+    private JMenu editMenu;
+	private JMenuItem editChangeFontMenuItem;
+    
+    private JMenu userMenu;
+    private JMenuItem userTransferFileMenuItem;
+    private JMenuItem userBlockMenuItem;
+    private JMenuItem userUnblockMenuItem;
+    private JMenuItem userInviteMenuItem;
+    
+    private JLabel nickNameLabel;
+    private JLabel statusLabel;
+    private JLabel personalMessageLabel;
+    private JScrollPane inputScrollPane;
+    private JTextArea inputTextArea;
+    private JScrollPane messageScrollPane;
+    private JTextArea messageTextArea;
+    
+    private JButton changeFontButton;
+	private JButton transferFileButton;
+	private JButton blockButton;
+	private JButton viewHistoryButton;
+	private JButton sendButton;
+	
+    private static final long serialVersionUID = 1L;
 	
 	public Conversation(User user, Contact contact) {
 		m_messages = new Vector<Message>();
@@ -14,6 +49,8 @@ public class Conversation {
 		
 		m_user = user;
 		m_participants.add(contact);
+		
+		initComponents();
 	}
 	
 	public int numberOfMessages() { return m_messages.size(); }
@@ -77,28 +114,309 @@ public class Conversation {
 		return true;
 	}
 	
-	// TODO: Finish me
 	public boolean sendMessage(String message) {
 		Message m = new Message(m_user, message);
 		m_messages.add(m);
 		
+		// TODO: Finish me
+		
 		return true;
 	}
 	
-	// TODO: Finish me
 	public boolean receiveMessage(String userName, long id, String message) {
 		if(userName == null || message == null) { return false; }
+		// TODO: Finish me
 		return receiveMessage(getParticipant(userName), id, message);
 	}
 	
-	// TODO: Finish me
 	public boolean receiveMessage(Contact c, long id, String message) {
 		if(c == null || message == null) { return false; }
 		
 		Message m = new Message(c, id, message);
 		m_messages.add(m);
 		
+		// TODO: Finish me
+		
 		return true;
 	}
 	
+    private void initComponents() {
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Message");
+        setMinimumSize(new Dimension(200, 150));
+        
+    	messageMenuBar = new JMenuBar();
+    	
+    	fileMenu = new JMenu();
+        fileSaveConversationMenuItem = new JMenuItem();
+        fileViewHistoryMenuItem = new JMenuItem();
+        fileCloseMenuItem = new JMenuItem();
+        
+        editMenu = new JMenu();
+        editChangeFontMenuItem = new JMenuItem();
+        
+        userMenu = new JMenu();
+        userTransferFileMenuItem = new JMenuItem();
+        userBlockMenuItem = new JMenuItem();
+        userUnblockMenuItem = new JMenuItem();
+        userInviteMenuItem = new JMenuItem();
+    	
+        nickNameLabel = new JLabel();
+        statusLabel = new JLabel();
+        personalMessageLabel = new JLabel();
+        inputScrollPane = new JScrollPane();
+        inputTextArea = new JTextArea();
+        messageScrollPane = new JScrollPane();
+        messageTextArea = new JTextArea();
+        
+        changeFontButton = new JButton();
+        transferFileButton = new JButton();
+        blockButton = new JButton();
+        viewHistoryButton = new JButton();
+        sendButton = new JButton();
+
+        inputTextArea.setColumns(20);
+        inputTextArea.setRows(5);
+        inputTextArea.setPreferredSize(new Dimension(164, 94));
+        inputTextArea.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent evt) {
+                inputTextAreaKeyPressed(evt);
+            }
+            public void keyReleased(KeyEvent evt) {
+                inputTextAreaKeyReleased(evt);
+            }
+        });
+        inputScrollPane.setViewportView(inputTextArea);
+
+        messageTextArea.setColumns(20);
+        messageTextArea.setRows(5);
+        messageScrollPane.setViewportView(messageTextArea);
+
+        changeFontButton.setText("Change Font");
+        changeFontButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                changeFontButtonActionPerformed(evt);
+            }
+        });
+
+        nickNameLabel.setText("Nickname");
+
+        statusLabel.setText("Status");
+
+        personalMessageLabel.setText("Personal Message");
+
+        viewHistoryButton.setText("View History");
+        viewHistoryButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                viewHistoryButtonActionPerformed(evt);
+            }
+        });
+
+        blockButton.setText("Block");
+        blockButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                blockButtonActionPerformed(evt);
+            }
+        });
+
+        sendButton.setText("Send");
+        sendButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                sendButtonActionPerformed(evt);
+            }
+        });
+
+        transferFileButton.setText("Transfer File");
+        transferFileButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                transferFileButtonActionPerformed(evt);
+            }
+        });
+
+        fileMenu.setText("File");
+
+        fileSaveConversationMenuItem.setText("Save Conversation");
+        fileSaveConversationMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                fileSaveConversationMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(fileSaveConversationMenuItem);
+
+        fileViewHistoryMenuItem.setText("View History");
+        fileViewHistoryMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                fileViewHistoryMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(fileViewHistoryMenuItem);
+
+        fileCloseMenuItem.setText("Close");
+        fileCloseMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                fileCloseMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(fileCloseMenuItem);
+
+        messageMenuBar.add(fileMenu);
+
+        editMenu.setText("Edit");
+
+        editChangeFontMenuItem.setText("Change Font");
+        editChangeFontMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                editChangeFontMenuItemActionPerformed(evt);
+            }
+        });
+        editMenu.add(editChangeFontMenuItem);
+
+        messageMenuBar.add(editMenu);
+
+        userMenu.setText("User");
+
+        userTransferFileMenuItem.setText("Transfer File");
+        userTransferFileMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                userTransferFileMenuItemActionPerformed(evt);
+            }
+        });
+        userMenu.add(userTransferFileMenuItem);
+
+        userBlockMenuItem.setText("Block");
+        userBlockMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                userBlockMenuItemActionPerformed(evt);
+            }
+        });
+        userMenu.add(userBlockMenuItem);
+
+        userUnblockMenuItem.setText("Unblock");
+        userUnblockMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                userUnblockMenuItemActionPerformed(evt);
+            }
+        });
+        userMenu.add(userUnblockMenuItem);
+
+        userInviteMenuItem.setText("Invite");
+        userInviteMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                userInviteMenuItemActionPerformed(evt);
+            }
+        });
+        userMenu.add(userInviteMenuItem);
+
+        messageMenuBar.add(userMenu);
+
+        setJMenuBar(messageMenuBar);
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(inputScrollPane, GroupLayout.DEFAULT_SIZE, 515, Short.MAX_VALUE)
+            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(changeFontButton, GroupLayout.DEFAULT_SIZE, 95, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(transferFileButton, GroupLayout.DEFAULT_SIZE, 95, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(blockButton, GroupLayout.DEFAULT_SIZE, 77, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(viewHistoryButton, GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(sendButton, GroupLayout.DEFAULT_SIZE, 68, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(nickNameLabel, GroupLayout.DEFAULT_SIZE, 387, Short.MAX_VALUE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(statusLabel, GroupLayout.PREFERRED_SIZE, 122, GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(personalMessageLabel, GroupLayout.DEFAULT_SIZE, 505, Short.MAX_VALUE)
+                .addContainerGap())
+            .addComponent(messageScrollPane, GroupLayout.DEFAULT_SIZE, 515, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(nickNameLabel)
+                    .addComponent(statusLabel, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(personalMessageLabel)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(messageScrollPane, GroupLayout.DEFAULT_SIZE, 196, Short.MAX_VALUE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(changeFontButton)
+                    .addComponent(sendButton)
+                    .addComponent(transferFileButton)
+                    .addComponent(blockButton)
+                    .addComponent(viewHistoryButton))
+                .addGap(1, 1, 1)
+                .addComponent(inputScrollPane, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE))
+        );
+
+        pack();
+    }
+
+    private void inputTextAreaKeyReleased(KeyEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void inputTextAreaKeyPressed(KeyEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void fileSaveConversationMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void fileViewHistoryMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void fileCloseMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void editChangeFontMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void userTransferFileMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void userBlockMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void userUnblockMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+    
+    private void userInviteMenuItemActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+    
+    private void changeFontButtonActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+    
+    private void transferFileButtonActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+    
+    private void blockButtonActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void viewHistoryButtonActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void sendButtonActionPerformed(ActionEvent evt) {
+        // TODO add your handling code here:
+    }
+    
 }
