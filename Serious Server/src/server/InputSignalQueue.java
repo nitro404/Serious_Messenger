02489364c8e17706b972028990b27cb2180ab661
Corey Faibish <hypnotiqbig@gmail.com>
2010-11-22 01:35:39 +0000

    Added Input and Output Queues for server side. (for real this time)
    git-svn-id: file:///e/SVN/serious_messenger@25 b1debafe-a605-4e1b-80f9-83a58a4e6cd1

---
+++ Serious Server/src/server/InputSignalQueue.java
@@ -0,0 +1,64 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.ArrayDeque;
+import signal.*;
+import shared.*;
+
+public class InputSignalQueue {
+	private ArrayDeque<Signal> m_inSignalQueue;
+	private DataInputStream m_in;
+	private DataOutputStream m_out;
+
+	public InputSignalQueue() {
+		m_inSignalQueue = new ArrayDeque<Signal>();
+	}
+
+	public void initialize(Server s, DataInputStream in, DataOutputStream out) {
+		m_in = in;
+		m_out = out;
+	}
+
+	public void addSignal(Signal s) {
+		if (s == null) {
+			return;
+		}
+
+		m_inSignalQueue.add(s);
+	}
+	
+	public void run(){//0,2,8,10,12,14
+		while (true){
+			if (!m_inSignalQueue.isEmpty()){
+				Signal s = m_inSignalQueue.remove();
+				
+				if (s.getSignalType() == SignalType.LoginRequest) {
+					LoginRequest s2 = (LoginRequest) s;
+					//do stuff
+				}	
+				else if (s.getSignalType() == SignalType.Logout) {
+					Logout s2 = (Logout) s;
+					//do stuff
+				}
+				else if (s.getSignalType() == SignalType.ChangePassword) {
+					ChangePassword s2 = (ChangePassword) s;
+					//do stuff
+				}
+				else if (s.getSignalType() == SignalType.AddContact) {
+					AddContact s2 = (AddContact) s;
+					//do stuff
+				}
+				else if (s.getSignalType() == SignalType.DeleteContact) {
+					DeleteContact s2 = (DeleteContact) s;
+					//do stuff
+				}
+				else if (s.getSignalType() == SignalType.BlockContact) {
+					BlockContact s2 = (BlockContact) s;
+					//do stuff
+				}
+			}
+		}
+	}
+}
+
---
+++ Serious Server/src/server/OutputSignalQueue.java
@@ -0,0 +1,57 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.ArrayDeque;
+import signal.*;
+import shared.*;
+
+public class OutputSignalQueue {
+	private ArrayDeque<Signal> m_outSignalQueue;
+	private DataInputStream m_in;
+	private DataOutputStream m_out;
+
+	public OutputSignalQueue() {
+		m_outSignalQueue = new ArrayDeque<Signal>();
+	}
+
+	public void initialize(Server s, DataInputStream in, DataOutputStream out) {
+		m_in = in;
+		m_out = out;
+	}
+
+	public void addSignal(Signal s) {
+		if (s == null) {
+			return;
+		}
+
+		m_outSignalQueue.add(s);
+	}
+
+	public void run() {
+		while (true) {
+			if (!m_outSignalQueue.isEmpty()) {
+				Signal s = m_outSignalQueue.remove();
+
+				if (s.getSignalType() == SignalType.LoginAuthenticated) {
+					// s.writeTo(clientOut);
+				} 
+				else if (s.getSignalType() == SignalType.PasswordChanged) {
+					// s.writeTo(clientOut);
+				} 
+				else if (s.getSignalType() == SignalType.ContactAdded) {
+					// s.writeTo(clientOut);
+				} 
+				else if (s.getSignalType() == SignalType.ContactDeleted) {
+					// s.writeTo(clientOut);
+				} 
+				else if (s.getSignalType() == SignalType.ContactBlocked) {
+					// s.writeTo(clientOut);
+				} 
+				else {
+					// unexpected signal
+				}
+			}
+		}
+	}
+}
